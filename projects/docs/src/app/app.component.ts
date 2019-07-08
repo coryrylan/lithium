2bc2ea3aabf6fce0402f50a239fe328f4b17dc7d
Cory Rylan <splintercode.cb@gmail.com>
2019-07-08 10:13:05 -0700

    fix routing animation bug

--- projects/docs/src/app/app.component.ts
+++ projects/docs/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterMetaDataService } from './common/services/router-metadata.service';
+import { fadeAnimation } from './common/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeAnimation]
 })
 export class AppComponent {
   constructor(private routerMetaDataService: RouterMetaDataService) {
--- projects/docs/src/app/common/animations.ts
+++ projects/docs/src/app/common/animations.ts
@@ -10,17 +10,12 @@ export const fadeAnimation = trigger('fadeAnimation', [
   transition('* => *', [
     query(
       ':enter',
-      [style({ opacity: 0 })],
-      { optional: true }
-    ),
-    query(
-      ':leave',
-      [style({ opacity: 1 }), animate('0.3s', style({ opacity: 0 }))],
+      [style({ opacity: 0, height: 0 })],
       { optional: true }
     ),
     query(
       ':enter',
-      [style({ opacity: 0 }), animate('0.3s', style({ opacity: 1 }))],
+      [style({ opacity: 0, height: 'auto' }), animate('0.4s', style({ opacity: 1 }))],
       { optional: true }
     )
   ])
