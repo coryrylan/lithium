2284377bca07ee877c159d42f4926bc91cbf4667
Cory Rylan <splintercode.cb@gmail.com>
2019-07-18 18:09:57 -0500

    fix: tree shaking issue with side effect imports

--- projects/lithium/public-api.ts
+++ projects/lithium/public-api.ts
@@ -2,8 +2,14 @@
  * Public API Surface of lithium
  */
 
+export * from './card/card';
+export * from './code-example/code-example';
+export * from './common/index';
 export * from './dropdown/dropdown';
-export * from './nav-bar/nav-bar';
+export * from './icons/index';
+export * from './loading-spinner/loading-spinner';
 export * from './modal/modal';
+export * from './nav-bar/nav-bar';
+export * from './progress-bar/progress-bar';
 export * from './side-nav/side-nav';
 export * from './tabs/tabs';
