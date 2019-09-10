189281c634cf0aafa18a95faaec0bf38527dfbfc
Cory Rylan <splintercode.cb@gmail.com>
2019-09-09 22:15:13 -0500

    test(progress-bar): add new unit tests

---
+++ projects/lithium/progress-bar/progress-bar.element.spec.ts
@@ -0,0 +1,32 @@
+import 'lithium-ui/progress-bar';
+import { LithiumProgressBar } from 'lithium-ui/progress-bar';
+import { createTestElement, waitForComponent, removeTestElement, componentIsStable, getComponentSlotContent } from 'lithium-ui/test/utils';
+
+describe('card element', () => {
+  let testElement: HTMLElement;
+  let component: LithiumProgressBar;
+
+  beforeEach(async () => {
+    testElement = createTestElement();
+    testElement.innerHTML = `
+      <li-progress-bar></li-progress-bar>
+    `;
+    await waitForComponent('li-progress-bar');
+    component = testElement.querySelector<LithiumProgressBar>('li-progress-bar');
+  });
+
+  afterEach(() => {
+    removeTestElement(testElement);
+  });
+
+  it('should render a progress bar', async () => {
+    component.value = 50;
+    await componentIsStable(component);
+    expect(component.shadowRoot.innerHTML.includes('li-progress-bar-value')).toBe(true);
+    expect(component.shadowRoot.innerHTML.includes('50%')).toBe(true);
+
+    component.showValue = false;
+    await componentIsStable(component);
+    expect(component.shadowRoot.innerHTML.includes('li-progress-bar-value')).toBe(false);
+  });
+});
