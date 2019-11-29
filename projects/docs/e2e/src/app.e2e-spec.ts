f7f389748f220ba09b5372b048fdd7a736bc8dbb
Cory Rylan <splintercode.cb@gmail.com>
2019-11-29 14:30:57 -0600

    chore: upgrade to latest angular rc
    Signed-off-by: Cory Rylan <splintercode.cb@gmail.com>

--- projects/docs/e2e/src/app.e2e-spec.ts
+++ projects/docs/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to docs!');
+    expect(page.getTitleText()).toEqual('docs app is running!');
   });
 
   afterEach(async () => {
